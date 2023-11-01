e87ac9ff9be27643c9606f9188cbc08fc147c94d
lukegarst <lukegarst@icloud.com>
2023-10-31 19:27:27 -0700

    added auth and CRUD functionality. Not sure if I will need all the CRUD routes yet.

--- backend/controllers/mediaController.js
+++ backend/controllers/mediaController.js
@@ -1,11 +1,15 @@
 const asyncHandler = require('express-async-handler')
 
+const Media = require('../models/mediaModel')
+const User = require('../models/userModel')
 
 // @desc    Show media
 // @route   GET /home
 // @access  Private
 const getMedia = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: 'Get media' })
+  const media = await Media.find({ user: req.user.id })
+
+  res.status(200).json(media)
 })
 
 // @desc    Add media
@@ -14,25 +18,77 @@ const getMedia = asyncHandler(async (req, res) => {
 const postMedia = asyncHandler(async (req, res) => {
   if(!req.body.text) {
     res.status(400)
-    throw new Error('Please select media')
+    throw new Error('Please add a text field')
   }
 
-  res.status(200).json({ message: 'Post media' })
+  const media = await Media.create({
+    text: req.body.text,
+    user: req.user.id
+  })
+
+  res.status(200).json(media)
 })
 
 // @desc    Update media
 // @route   PUT /home/:id
 // @access  Private
 const updateMedia = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: `Update media ${req.params.id}` })
-})
+  const media = await Media.findById(req.params.id)
 
+  if(!media) {
+    res.status(400)
+    throw new Error('media not found')
+  }
+
+  const user = await User.findById(req.user.id)
+
+  //Check for user
+  if(!user) {
+    res.status(401)
+    throw new Error('User not found')
+  }
+
+  //Make sure the logged in user matches the media user
+  if(media.user.toString() !== user.id) {
+    res.status(401)
+    throw new Error('User not authorized')
+  }
+
+    const updatedMedia = await Media.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+  })
+
+  res.status(200).json(updatedMedia)
+})
 
 // @desc    Delete media
 // @route   DELETE /home/:id
 // @access  Private
 const deleteMedia = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: `Delete media ${req.params.id}` })
+  const media = await Media.findById(req.params.id)
+
+  if(!media) {
+    res.status(400)
+    throw new Error('media not found')
+  }
+
+  const user = await User.findById(req.user.id)
+
+  //Check for user
+  if(!user) {
+    res.status(401)
+    throw new Error('User not found')
+  }
+
+  //Make sure the logged in user matches the media user
+  if(media.user.toString() !== user.id) {
+    res.status(401)
+    throw new Error('User not authorized')
+  }
+
+  await media.deleteOne()
+
+  res.status(200).json({ id: req.params.id })
 })
 
 module.exports = {
--- backend/routes/mediaRoutes.js
+++ backend/routes/mediaRoutes.js
@@ -1,9 +1,16 @@
 const express = require('express')
 const router = express.Router()
-const { getMedia, postMedia, updateMedia, deleteMedia } = require('../controllers/mediaController')
+const { 
+  getMedia, 
+  postMedia, 
+  updateMedia, 
+  deleteMedia,
+} = require('../controllers/mediaController')
 
-router.route('/').get(getMedia).post(postMedia)
+const {protect} = require('../middleware/authMiddleware')
 
-router.route('/:id').put(updateMedia).delete(deleteMedia)
+router.route('/').get(protect, getMedia).post(protect, postMedia)
+
+router.route('/:id').put(protect, updateMedia).delete(protect, deleteMedia)
 
 module.exports = router
